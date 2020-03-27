d9f82849f639c8573f772cdf944ca79c76e7aa38
JUAN M RESTREPO <tinto922@gmail.com>
2020-03-27 12:16:36 +0000

    Automatically backed up by Learn

--- my_first_hash.rb
+++ my_first_hash.rb
@@ -11,7 +11,8 @@ def shipping_manifest
   # We have 5 whale bone corsets, 2 porcelain vases and 3 oil paintings 
   
   the_manifest = {
-    ""
+    "whale bone corsets" => 5,
+    " "
     
   }
 
